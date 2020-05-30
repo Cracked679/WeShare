da064dd340b16d6fdbd373ecea7871641f17bf2f
/ <31470255+Cracked679@users.noreply.github.com>
2020-05-30 14:25:55 +0530

    Initializing app and implemented few things at first.
    Completed Follwing Things:
    
    1)Express and Formatting Response
    2)User Registration
    3)Validation and Login Routes
    4)Authentication Middleware

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: "user",
+            body: "Thi is Scream body",
+            createdAt: "2020-05-27T15:26:18.678Z",
+            likeCount: 5,
+            commentCount:2   
+        }
+    ]
+}
