d836d2cd0422b946895a1fd2dcf23cc04da2d884
Cracked679 <shyambodke679@gmail.com>
2020-06-05 20:18:51 +0530

    Upload Image

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { signup, login ,uploadImage  } = require('./handlers/users');
 
 //Scream Routes
 app.get('/screams', getAllScreams);
@@ -14,5 +14,6 @@ app.post('/scream', FBAuth, postOneScream);
 //Users Route
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth,uploadImage);
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
--- socialape-functions/functions/util/admin.js
+++ socialape-functions/functions/util/admin.js
@@ -1,9 +1,10 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('C:/Users/Cracked/Desktop/SocialApe/Credientials/socialape-9c923-4b7948c22f7d.json');
+const serviceAccount = require('/home/cracked/Desktop/Projects/SocialApe/Credientials/socialape-9c923-4b7948c22f7d.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    storageBucket: "socialape-9c923.appspot.com",
     databaseURL: "https://socialape-9c923.firebaseio.com"
 });
 
