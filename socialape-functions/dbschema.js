a40fb43bba90264a282c91f15560b37ecee757e9
Cracked679 <shyambodke679@gmail.com>
2020-06-06 10:21:52 +0530

    Add And Get User Profile Details

--- socialape-functions/dbschema.js
+++ socialape-functions/dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: '',
+            email: 'user@gmail.com',
+            handle: 'user',
+            createdAt: '2020-05-27T15:26:18.678Z',
+            imageUrl: 'image/dfadfaasdasd/asdsadac',
+            bio: 'Hello,my name is user,nice to meet you',
+            website: 'https://user.com',
+            location: 'Nashik,India'
+        }
+    ],
     screams: [
         {
             userHandle: "user",
@@ -6,6 +18,29 @@ let db = {
             createdAt: "2020-05-27T15:26:18.678Z",
             likeCount: 5,
             commentCount:2   
+        }
+    ]
+};
+
+const userDetails = {
+    credentials : {
+        userId: 'aasdkjasdkjasdjkl',
+        email: 'user@email.com',
+        handle:'user',
+        createdAt: '2020-05-27T15:26:18.678Z',
+        imageurl: 'image/asdjasjdnj/asdhasdasd',
+        bio: 'Hello, my name is user,nice to meet you',
+        website: 'https://user.com',
+        location: ' Nashik,India'
+    },
+    likes:[
+        {
+            userHandle: 'user',
+            screamId: 'jajksdkhkk'
+        },
+        {
+            userHandle:'user',
+            screamId: 'asdllajsnldnknasl'
         }
     ]
 }
