426de130830d767d873658962bd23ccf540b7301
Cracked679 <shyambodke679@gmail.com>
2020-06-09 10:53:24 +0530

    Create And Get Notifications

--- socialape-functions/dbschema.js
+++ socialape-functions/dbschema.js
@@ -1,54 +1,64 @@
 let db = {
   users: [
     {
-      userId   : "",
-      email    : "user@gmail.com",
-      handle   : "user",
+      userId: "",
+      email: "user@gmail.com",
+      handle: "user",
       createdAt: "2020-05-27T15:26:18.678Z",
-      imageUrl : "image/dfadfaasdasd/asdsadac",
-      bio      : "Hello,my name is user,nice to meet you",
-      website  : "https://user.com",
-      location : "Nashik,India",
+      imageUrl: "image/dfadfaasdasd/asdsadac",
+      bio: "Hello,my name is user,nice to meet you",
+      website: "https://user.com",
+      location: "Nashik,India",
     },
   ],
   screams: [
     {
-      userHandle  : "user",
-      body        : "Thi is Scream body",
-      createdAt   : "2020-05-27T15:26:18.678Z",
-      likeCount   : 5,
+      userHandle: "user",
+      body: "Thi is Scream body",
+      createdAt: "2020-05-27T15:26:18.678Z",
+      likeCount: 5,
       commentCount: 2,
     },
   ],
   comments: [
     {
       userHandle: "user",
-      screamId  : "sdnjansdnansjn",
-      body      : "nice one mate",
-      createdAt : "2020-05-27T15:26:18.678Z",
+      screamId: "sdnjansdnansjn",
+      body: "nice one mate",
+      createdAt: "2020-05-27T15:26:18.678Z",
+    },
+  ],
+  notifications: [
+    {
+      recipient: "user",
+      sender: "john",
+      read: "true | false",
+      screamId: "jashjkdbkbjkbajksd",
+      type: "like| comment",
+      createdAt: "2020-05-27T15:26:18.678Z",
     },
   ],
 };
 
 const userDetails = {
   credentials: {
-    userId   : "aasdkjasdkjasdjkl",
-    email    : "user@email.com",
-    handle   : "user",
+    userId: "aasdkjasdkjasdjkl",
+    email: "user@email.com",
+    handle: "user",
     createdAt: "2020-05-27T15:26:18.678Z",
-    imageurl : "image/asdjasjdnj/asdhasdasd",
-    bio      : "Hello, my name is user,nice to meet you",
-    website  : "https://user.com",
-    location : " Nashik,India",
+    imageurl: "image/asdjasjdnj/asdhasdasd",
+    bio: "Hello, my name is user,nice to meet you",
+    website: "https://user.com",
+    location: " Nashik,India",
   },
   likes: [
     {
       userHandle: "user",
-      screamId  : "jajksdkhkk",
+      screamId: "jajksdkhkk",
     },
     {
       userHandle: "user",
-      screamId  : "asdllajsnldnknasl",
+      screamId: "asdllajsnldnknasl",
     },
   ],
 };
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
-
 const app = require("express")();
-
 const FBAuth = require("./util/fbAuth");
+
+const { db } = require("./util/admin");
 
 const {
   getAllScreams,
@@ -39,3 +39,72 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("asia-east2")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions
+  .region("asia-east2")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error();
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("asia-east2")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
