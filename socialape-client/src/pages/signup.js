0c4b268aa4209f6b6977bc46964dff03948f2eb3
Cracked679 <shyambodke679@gmail.com>
2020-06-11 19:02:52 +0530

    Getting started with React

---
+++ socialape-client/src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SignUp Page</h1>
+      </div>
+    );
+  }
+}
+
+export default signup;
